23a2b448c806f60a6bee8230d7b4a564b2163243
Rafiul <hredoy.rafiul@gmail.com>
2023-08-01 18:02:16 +0600

    useState demo

--- src/App.js
+++ src/App.js
@@ -1,12 +1,15 @@
 import React from "react";
 //in App.js  file the component is acommodatd and the value of property has been passed, the properties are defined in
 // in Card.js file and propermapping has also been defined there
-import HOOKS_USESTATE from "./HOOKS_USESTATE/index1";
-
-
+import HOOKS_USESTATE1 from "./HOOKS_USESTATE/index1";
+import HOOKS_USESTATE2 from "./HOOKS_USESTATE/index2";
 
 function App() {
-  return <div><HOOKS_USESTATE/></div>;
+  return (
+    <div>
+      <HOOKS_USESTATE2 />
+    </div>
+  );
 }
 
 export default App;
