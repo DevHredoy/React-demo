9f64f9d76b2224184ac811453e7f0996c2122138
Rafiul Islam Hredoy <64517450+Kaka-Co@users.noreply.github.com>
2023-08-13 11:06:41 +0600

    controlling form element using state

--- src/App.js
+++ src/App.js
@@ -1,13 +1,11 @@
 import React from "react";
 //in App.js  file the component is acommodatd and the value of property has been passed, the properties are defined in
 // in Card.js file and propermapping has also been defined there
-import HOOKS_USESTATE1 from "./HOOKS_USESTATE/index1";
-import HOOKS_USESTATE2 from "./HOOKS_USESTATE/index2";
-
+import FORM from "./components/FORM/FORM";
 function App() {
   return (
     <div>
-      <HOOKS_USESTATE2 />
+      <FORM />
     </div>
   );
 }
