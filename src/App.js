b112a5b080f08793ac0bc494903bcc5c3bf30568
Rafiul Islam Hredoy <64517450+Kaka-Co@users.noreply.github.com>
2023-08-13 18:22:59 +0600

    demo of prop object and it's and it's properties,demo of state lifting from child to parent

--- src/App.js
+++ src/App.js
@@ -1,11 +1,19 @@
 import React from "react";
 //in App.js  file the component is acommodatd and the value of property has been passed, the properties are defined in
 // in Card.js file and propermapping has also been defined there
-import FORM from "./components/FORM/FORM";
+import Child from "./components/STATELIFTING/Child";
+const data = "I am from parent (app)";
+
+
+const handleChildData = (childData) => {
+  console.log("function value " + childData);
+  
+};
+
 function App() {
   return (
     <div>
-      <FORM />
+      <Child data={data} handleChildData={handleChildData} />
     </div>
   );
 }
