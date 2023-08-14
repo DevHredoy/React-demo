d7f9da55ee8945a06c80def2a57c182aec1f3df7
Rafiul Islam Hredoy <64517450+Kaka-Co@users.noreply.github.com>
2023-08-14 17:56:32 +0600

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,19 +1,14 @@
 import React from "react";
-//in App.js  file the component is acommodatd and the value of property has been passed, the properties are defined in
-// in Card.js file and propermapping has also been defined there
-import Child from "./components/STATELIFTING/Child";
-const data = "I am from parent (app)";
 
+import Home from "./components/STATELIFTING/Home";
 
-const handleChildData = (childData) => {
-  console.log("function value " + childData);
   
-};
+
 
 function App() {
   return (
     <div>
-      <Child data={data} handleChildData={handleChildData} />
+      <Home/>
     </div>
   );
 }
---
+++ src/components/STATELIFTING/Home.js
@@ -0,0 +1,8 @@
+import React from "react";
+
+const Home=()=> {
+    const todos=["todo1","todo2"];
+  return <div>I am in Home</div>;
+}
+
+export default Home;
